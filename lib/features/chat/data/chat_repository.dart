9d4cac52526e7125dd39697301be3a63a9d2db41
Abdur Rahman <rahman.abdur@egeneration.co>
2025-04-16 12:14:59 +0600

    merge fix

--- lib/features/chat/data/chat_repository.dart
+++ lib/features/chat/data/chat_repository.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+import 'package:bjp_app/features/chat/presentation/domain/chat_list_model.dart';
 import 'package:bjp_app/features/chat/presentation/domain/chat_model.dart';
+import 'package:bjp_app/features/events/domain/event_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -28,7 +31,7 @@ class ChatRepository {
     required Dio dioClient,
     required FlutterSecureStorage secureStorage,
   }) : _dioClient = dioClient,
-       _secureStorage = secureStorage;
+        _secureStorage = secureStorage;
 
   FutureEither<List<ChatModel>> fetchChatMessages(String partner_id) async {
     try {
@@ -58,12 +61,57 @@ class ChatRepository {
         // Access the "data" key, which contains the list of events
         final List<dynamic> chatList = response.data['data'];
         // Map the list to ChatModel objects
-        final chatModelList =
-            chatList
-                .map(
-                  (event) => ChatModel.fromMap(event as Map<String, dynamic>),
-                )
-                .toList();
+        final chatModelList = chatList
+            .map((event) => ChatModel.fromMap(event as Map<String, dynamic>))
+            .toList();
+        lgr.i('got chatModelList: $chatModelList');
+        return right(chatModelList);
+      } else {
+        return left(Failure('চ্যাট বার্তা গুলি পাওয়া যায়নি'));
+      }
+    } on DioException catch (err) {
+      if (err.response != null && err.response!.statusCode == 401) {
+        lgr.w(
+          '${err.response!.statusCode} ${err.response!.statusMessage}\n${err.response!.data}',
+        );
+      }
+      return left(Failure('You are not authorized to view this content'));
+    } catch (err) {
+      lgr.e('Error fetching chat messages: $err');
+      return left(Failure('চ্যাট বার্তা গুলি পাওয়া যায়নি'));
+    }
+  }
+  FutureEither<List<ChatList>> fetchChatList() async {
+    try {
+      final uri = Uri(
+        scheme: 'http',
+        port: ApiConstants.port,
+        host: ApiConstants.baseUrl,
+        path: ApiConstants.getAllChats,
+      );
+      lgr.i('fetching chat messages from: ${uri.toString()}');
+      final token = await _secureStorage.read(key: 'token');
+      lgr.e('token: $token');
+      lgr.i('sending request to fetch chat messages to ${uri.toString()}');
+      final response = await _dioClient.get(
+        uri.toString(),
+        options: Options(
+          headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer $token',
+          },
+        ),
+      );
+      lgr.i('got response: $response');
+
+      if (response.statusCode == 200) {
+        lgr.i('statusCode: 200 : ${response.data}');
+        // Access the "data" key, which contains the list of events
+        final List<dynamic> chatList = response.data['data'];
+        // Map the list to ChatModel objects
+        final chatModelList = chatList
+            .map((event) => ChatList.fromJson(event as Map<String, dynamic>))
+            .toList();
         lgr.i('got chatModelList: $chatModelList');
         return right(chatModelList);
       } else {
@@ -82,45 +130,49 @@ class ChatRepository {
     }
   }
 
-  FutureEitherVoid sendMessage(String receiverId, String text) async {
-    try {
-      final uri = Uri(
-        scheme: 'http',
-        port: ApiConstants.port,
-        host: ApiConstants.baseUrl,
-        path: ApiConstants.sendMessages,
-      );
-      final token = await _secureStorage.read(key: 'token');
-      lgr.w('got token: $token');
-      final result = await _dioClient.post(
-        uri.toString(),
-        data: {'receiver_id': receiverId, 'message': text},
+  FutureEitherVoid  sendMessage(String receiverId, String text) async {
+      try {
+        final uri = Uri(
+          scheme: 'http',
+          port: ApiConstants.port,
+          host: ApiConstants.baseUrl,
+          path: ApiConstants.sendMessages,
+        );
+        final token = await _secureStorage.read(key: 'token');
+        lgr.w('got token: $token');
+        final result = await _dioClient.post(
+          uri.toString(),
+          data: {'receiver_id': receiverId,
+            'message': text},
 
-        options: Options(
-          headers: {
-            'Accept': 'application/json',
-            'Authorization': 'Bearer $token',
-          },
-        ),
-      );
+          options: Options(
+            headers: {
+              'Accept': 'application/json',
+              'Authorization': 'Bearer $token',
+            },
+          ),
+        );
 
-      lgr.i('got response: $result');
+        lgr.i('got response: $result');
 
-      if (result.statusCode == 200) {
-        lgr.i('statusCode: 200 : ${result.data}');
+        if (result.statusCode == 200) {
+          lgr.i('statusCode: 200 : ${result.data}');
 
-        return right(null);
-      } else if (result.statusCode == 201) {
-        lgr.i('statusCode: 201 : ${result.data}');
+          return right(null);
+        }
+        else if (result.statusCode == 201){
+          lgr.i('statusCode: 201 : ${result.data}');
 
-        return right(null);
-      } else {
-        lgr.f('statusCode: ${result.statusCode} : ${result.data}');
+          return right(null);
+        }
+      else {
+          lgr.f('statusCode: ${result.statusCode} : ${result.data}');
+          return left(Failure('Failed to send message'));
+        }
+      } catch (err) {
+        lgr.e('error: $err');
         return left(Failure('Failed to send message'));
       }
-    } catch (err) {
-      lgr.e('error: $err');
-      return left(Failure('Failed to send message'));
-    }
+
   }
 }
